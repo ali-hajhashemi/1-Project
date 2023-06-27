d9f023722385726c6f78d30abd0d3602558dc5aa
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2023-06-27 17:33:04 +0330

    95% Complet

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
 $(document).ready(function () {
-  $(".navbar-navigation-links ul li a").click(function (e) {
-    e.preventDefault();
-
-    var currentUl = $(this).parent().find("ul");
+  // Header Dropdown Menu
+  $(".navbar-navigation-links > ul > li > a").click(function (e) {
+    var currentUl = $(this).parent().children("ul");
     var status = null;
-
+    e.preventDefault();
     if ($(currentUl).is(":visible")) {
       status = true;
     } else {
       status = false;
     }
 
-    $(".navbar-navigation-links ul li > ul").hide();
+    $(".navbar-navigation-links > ul > li > ul").slideUp();
 
     if (status) {
-      $(currentUl).hide();
+      $(currentUl).slideUp();
     } else {
-      $(currentUl).show();
+      $(currentUl).slideDown();
     }
+  });
+
+  // Scroll Header System
+  $(window).scroll(function () {
+    var header_offset = $(window).scrollTop();
+
+    console.log(header_offset);
+
+    if (header_offset > 0) {
+      // Add Class
+      $("#navbar").addClass("navbar-withe");
+    } else {
+      // Remove Class
+      $("#navbar").removeClass("navbar-withe");
+    }
+  });
+
+  $(".navbar-menu-mobile").click(function name(params) {
+    $(".navbar-navigation-elements").slideToggle(400);
+    $("#navbar").toggleClass("navbar-withe");
   });
 });
