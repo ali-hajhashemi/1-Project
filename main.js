c70696e59c0ca7a5265e4eb25dd7d84048977e81
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2023-06-28 19:44:54 +0330

    Complet Main Page

--- main.js
+++ main.js
@@ -19,23 +19,63 @@ $(document).ready(function () {
     }
   });
 
+  scroll_load();
+
   // Scroll Header System
   $(window).scroll(function () {
-    var header_offset = $(window).scrollTop();
-
-    console.log(header_offset);
-
-    if (header_offset > 0) {
-      // Add Class
-      $("#navbar").addClass("navbar-withe");
-    } else {
-      // Remove Class
-      $("#navbar").removeClass("navbar-withe");
-    }
+    scroll_load();
   });
 
   $(".navbar-menu-mobile").click(function name(params) {
     $(".navbar-navigation-elements").slideToggle(400);
-    $("#navbar").toggleClass("navbar-withe");
+  });
+
+  /* Show Case Slide Changer
+  =================================================================== */
+
+  // Variables
+  var showcase_slide = $(".showcase-slide");
+  var slide_changer_left = $(".showcase-slide-changer-left a");
+  var slide_changer_right = $(".showcase-slide-changer-right a");
+  var i = 0;
+
+  $(showcase_slide).hide();
+  $(showcase_slide[0]).show();
+
+  // Left Side Click
+  $(slide_changer_right).click(function (e) {
+    e.preventDefault();
+    $(showcase_slide).hide();
+    i++;
+    if (i >= showcase_slide.length) {
+      i = 0;
+    }
+    $(showcase_slide[i]).fadeToggle();
+  });
+
+  $(slide_changer_left).click(function (e) {
+    e.preventDefault();
+    $(showcase_slide).hide();
+    i--;
+    if (i < 0) {
+      i = showcase_slide.length - 1;
+    }
+    $(showcase_slide[i]).fadeToggle();
   });
 });
+
+/* Functions
+====================================================================== */
+
+// Scroll Refresh Load
+function scroll_load() {
+  var header_offset = $(window).scrollTop();
+
+  if (header_offset > 0) {
+    // Add Class
+    $("#navbar").addClass("navbar-withe");
+  } else {
+    // Remove Class
+    $("#navbar").removeClass("navbar-withe");
+  }
+}
